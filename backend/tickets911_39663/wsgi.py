fb513fd1b1e057a97574dc1b65e535c0f58268a7
Crowdbotics <team@crowdbotics.com>
2023-04-02 16:45:06 +0000

    Initial commit.

---
+++ backend/tickets911_39663/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tickets911_39663 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tickets911_39663.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tickets911-39663",
+  url: "https://tickets911-39663.botics.co",
+  api: "https://tickets911-39663.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
